9bdef2bcdd265a730ef8f35cb4bc7c35af22e4ae
Sergey Kravchuk <kravchuk@icloud.com>
2020-01-14 13:50:22 +0300

    Initial Commit

---
+++ Skillbox_m3 (RxSwift)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skillbox_m3 (RxSwift)
+//
+//  Created by Kravchuk Sergey on 14.01.2020.
+//  Copyright © 2020 Kravchuk Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
